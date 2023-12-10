96b4000f382186ece5744dbb0c5089e89b3828dd
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-10 20:33:22 -0300

    Remove next image

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -1,7 +1,7 @@
-import Image from 'next/image'
 
 export default function Home() {
   return (
-      <p>This a Discord Clone</p>
+      <p className="Text-3xl font-bold text-indigo-500"> 
+      This a Discord Clone</p>
   )
 }
