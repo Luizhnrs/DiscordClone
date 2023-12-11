f1565ec5de1e49c1908f2696a6bf634b50580ad0
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-11 14:40:48 -0300

    Learning Next End

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
 
 export default function Home() {
   return (
     <div className="flex flex-col"> 
       <p className="Text-3xl font-bold text-indigo-500"> 
       This a Discord Clone</p>
-      <Button> 
+      <Button>
         Click On Me
         </Button>
       </div>
