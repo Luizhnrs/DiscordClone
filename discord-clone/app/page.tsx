d8ba687b5b0a391d0b7eb48c8cde699693b7c2c8
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-11 14:34:28 -0300

    Button Tests

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    <div className="flex flex-col"> 
       <p className="Text-3xl font-bold text-indigo-500"> 
       This a Discord Clone</p>
+      <Button> 
+        Click On Me
+        </Button>
+      </div>
   )
 }
