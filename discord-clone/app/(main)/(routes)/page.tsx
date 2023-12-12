0bbbbea250fa48ca1d1a55c106fb7b124c03cc42
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-12 16:45:24 -0300

    Main page Login Informations

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,15 +1,14 @@
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { UserButton } from "@clerk/nextjs";
 
 
 export default function Home() {
   return (
-    <div className="flex flex-col"> 
-      <p className="Text-3xl font-bold text-indigo-500"> 
-      This a Discord Clone</p>
-      <Button>
-        Click On Me
-        </Button>
+    <div>
+      <UserButton
+        afterSignOutUrl="/"
+      />
       </div>
   )
 }
