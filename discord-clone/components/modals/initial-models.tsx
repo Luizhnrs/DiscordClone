036998a2d506ce595d1dbf8ef6278f9a3902beee
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-15 17:30:46 -0300

    Feat: Customize your server

--- discord-clone/components/modals/initial-models.tsx
+++ discord-clone/components/modals/initial-models.tsx
@@ -12,10 +12,15 @@ import{
 export const InitialModal = () => {
     return(
         <Dialog open>
-            <DialogContent className="bg-White text-black p-0 
+            <DialogContent className="bg-white text-black p-0 
             overflow-hidden">
-                <DialogHeader>
+                <DialogHeader className="pt-8 px-6">
+                  <DialogTitle className="text-2xl text-center font-bold">
                     Customize Your Server
+                  </DialogTitle>
+                  <DialogDescription className="text-center text-zinc-500">
+                    Leave your server with your face, with a name, an image, remembering that you can change this later
+                  </DialogDescription>
                 </DialogHeader>
             </DialogContent>
         </Dialog>
