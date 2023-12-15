fe196e3ef71875c2f668719a415360105ade1f75
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-15 18:23:54 -0300

    Feat: Server Name and Image Req

--- discord-clone/components/modals/initial-models.tsx
+++ discord-clone/components/modals/initial-models.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
 import{
     Dialog,
     DialogContent,
@@ -9,7 +13,24 @@ import{
     DialogTitle
 } from "@/components/ui/dialog"
 
+const fromSchema = z.object({
+    name: z.string().min(1,{
+        message:"Server Name is Required"
+    }),
+    imageUrl: z.string().min(1,{
+        message: "Server Image is Required"
+    })
+})
+
+
 export const InitialModal = () => {
+    const form = useForm({
+        defaultValues:{
+            name:"",
+            imageUrl:"",
+        }
+    });
+
     return(
         <Dialog open>
             <DialogContent className="bg-white text-black p-0 
