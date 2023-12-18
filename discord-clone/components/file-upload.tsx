3a435b9a0df3e77a72c3c5be79bdb3db1fd7e767
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-17 22:22:23 -0300

    Feat: File Dropzone Complete Integration

--- discord-clone/components/file-upload.tsx
+++ discord-clone/components/file-upload.tsx
@@ -23,7 +23,8 @@ export const FileUpload = ({
        <UploadDropzone
         endpoint={endpoint}
         onClientUploadComplete={(res)=> {
-            onChange(res?.[0].url);
+            onChange(res?.[0]
+                .url);
         }}
         onUploadError={(error: Error)=> {
             console.log(error);
