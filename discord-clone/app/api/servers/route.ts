35d70895aded151e06da9c4cea25bd850b5af08f
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-19 13:39:42 -0300

    Feat: Server Name

--- discord-clone/app/api/servers/route.ts
+++ discord-clone/app/api/servers/route.ts
@@ -1,3 +1,4 @@
+import {v4 as uuidv4} from"uuid";
 import { error } from 'console';
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
@@ -11,6 +12,20 @@ export async function POST(req: Request){
         if(!profile){
             return new NextResponse("Unauthorized", {status:401})
         }
+
+        const server = await db.server.create({
+            data:{
+                profileId: profile.id,
+                name,
+                imageUrl,
+                inviteCode: uuidv4(),
+                channels:{
+                    create:[
+                        {name: "general", profileId: profile.id}
+                    ]
+                }
+            }
+        })
     } catch(error){
         console.log("[SERVERS_POST]", error);
         return new NextResponse("Internal Error", {status:500});
