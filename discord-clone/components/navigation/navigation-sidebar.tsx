f3d37a6e45b485bfd4c90bb2bf13e76ce6ec20d5
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-22 16:28:06 -0300

    Feat: Separator Import

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
 
 export const NavigationSideBar = async () => {
   const profile = await currentProfile();
@@ -23,6 +24,7 @@ export const NavigationSideBar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
         <NavigationAction/>
+        <Separator/>
     </div>
   );
 };
