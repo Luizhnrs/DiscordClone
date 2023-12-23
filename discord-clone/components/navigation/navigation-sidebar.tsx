6a7db5a059a0d7dd61629244283589d463f58af9
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-23 18:07:32 -0300

    Import: Navigation Item Server

--- discord-clone/components/navigation/navigation-sidebar.tsx
+++ discord-clone/components/navigation/navigation-sidebar.tsx
@@ -4,6 +4,7 @@ import { db } from "@/lib/db";
 import { NavigationAction } from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSideBar = async () => {
   const profile = await currentProfile();
@@ -24,17 +25,19 @@ export const NavigationSideBar = async () => {
 
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
-        <NavigationAction/>
-        <Separator
-          className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
-        />
-        <ScrollArea  className="flex-1 w-full" >
-          {servers.map((server)=> (
-            <div key={server.id} className="mb-4">
-              <NavigationItem/>
-            </div>
-          ))}
-        </ScrollArea>
+      <NavigationAction />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div key={server.id} className="mb-4">
+            <NavigationItem
+              id={server.id}
+              name={server.name}
+              imageUrl={server.imageUrl}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
