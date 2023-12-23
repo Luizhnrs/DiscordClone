f0cc62c33d7a361537efd51da8e425f9157349a7
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-23 18:49:45 -0300

    Correction: Import Params

--- discord-clone/components/navigation/navigation-item.tsx
+++ discord-clone/components/navigation/navigation-item.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import Image from "next/image";
-import { useParams, useParams, useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
