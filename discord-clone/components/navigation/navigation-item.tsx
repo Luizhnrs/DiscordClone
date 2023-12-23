57d5c45f728bc9c9353e3e20a836e25efbc0f010
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-23 16:21:49 -0300

    Feat: Interface NavigationItemProps ADD

--- discord-clone/components/navigation/navigation-item.tsx
+++ discord-clone/components/navigation/navigation-item.tsx
@@ -5,3 +5,10 @@ import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+
+
+interface NavigationItemProps{
+    id: string;
+    imageUrl: string;
+    name: string;
+}
