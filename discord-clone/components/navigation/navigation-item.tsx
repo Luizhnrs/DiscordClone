51a03b993127afdef45db7fe98ed566c50ff5c0f
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-23 19:38:48 -0300

    Feat: Second Div on Button in Nav Item

--- discord-clone/components/navigation/navigation-item.tsx
+++ discord-clone/components/navigation/navigation-item.tsx
@@ -5,6 +5,7 @@ import { useParams, useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
 import { ActionTooltip } from "@/components/action-tooltip";
+import { group } from "console";
 
 
 interface NavigationItemProps{
@@ -35,9 +36,14 @@ export const NavigationItem = ({
                 "absolute left-0 bg-primary rounded-r-full transition-all w-[4px]",
                 params?.serverId !== id && "group-hover:h[20px]",
                 params?.serverId === id ? "h-[36px]" : "h-[80px]"
+            )}/>
+           <div className={cn(
+            "relative group flex mx-3 h-[48px] w-[48px] rounded-[24px group-hover:rounded-[16px] transition-all overflow-hidden", 
+            params?.serverId === id && "bg-primary/10 text primary rounded-[16px]"      
             )}
-           >
 
+           >
+                
            </div>
         </button>
     </ActionTooltip>
