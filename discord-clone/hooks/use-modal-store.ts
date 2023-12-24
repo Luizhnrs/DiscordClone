ba13b1a38a29b60d94e1121a68bc65d861495615
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-24 20:53:14 -0300

    feat: ModalStore Interface

--- discord-clone/hooks/use-modal-store.ts
+++ discord-clone/hooks/use-modal-store.ts
@@ -1,3 +1,11 @@
+import { ModelArg } from '@prisma/client/runtime/library';
+import { modalType } from './use-modal-store';
 import {create} from "zustand";
 
-export type modalType = "Create Server"
+export type modalType = "Create Server";
+
+interface modalStore{
+    type: modalType | null;
+    isOpen: Boolean;
+    onOpen: (type: ModelType) => void;
+}
