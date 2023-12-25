bb0082f825de7808eb39c64e7cd7990152c2663a
Luizhnrs <Luizhnrsg@gmail.com>
2023-12-24 21:02:18 -0300

    Feat: useModal Create

--- discord-clone/hooks/use-modal-store.ts
+++ discord-clone/hooks/use-modal-store.ts
@@ -1,11 +1,20 @@
 import { ModelArg } from '@prisma/client/runtime/library';
 import { modalType } from './use-modal-store';
 import {create} from "zustand";
+import { date, set } from 'zod';
 
 export type modalType = "Create Server";
 
 interface modalStore{
     type: modalType | null;
     isOpen: Boolean;
-    onOpen: (type: ModelType) => void;
+    onOpen: (type: modalType) => void;
+    onClose: () => void;
 }
+
+export const useModal = create<modalStore>((set)=> ({
+    type: null,
+    isOpen: false,
+    onOpen: (type) => set({isOpen: true, type}),
+    onClose: () => set({type: null, isOpen:false})
+}))
